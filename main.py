a5701b5d1f39cdf4dd67d2ba0cc959f511341fbd
Heckler <77711811+Programming-geek582@users.noreply.github.com>
2023-07-04 12:53:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,23 @@ app.secret_key = "werkzeugIsChad"
 @app.before_serving
 async def before_startup():
     await Tortoise.init(
-        db_url="sqlite://passwords.sqlite",
+        db_url="postgres://pg:Qun72wh5L4lso1M6Kqa3Pq9HQM3fDh7N@dpg-cihsf8qip7vpelqibkog-a.oregon-postgres.render.com/passwordmanager_3dzy",
         modules={
             'models' : ['models']
-        }
+        },
+        config='connections': {
+                          # Dict format for connection
+                          'default': {
+                              'engine': 'tortoise.backends.asyncpg',
+                              'credentials': {
+                                  'host': 'dpg-cihsf8qip7vpelqibkog-a',
+                                  'port': '5432',
+                                  'user': 'pg',
+                                  'password': 'Qun72wh5L4lso1M6Kqa3Pq9HQM3fDh7N',
+                                  'database': 'passwordmanager_3dzy',
+                              },
+                              'maxsize': 100
+                          }
     )
     await Tortoise.generate_schemas()
     await general.register(app)
